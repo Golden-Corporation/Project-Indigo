c5d5c15781eb30fadd91d274527f378dffdb7370
GoldFirey <81136576+GoldFirey124YT@users.noreply.github.com>
2023-06-26 02:04:58 +1000

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -1,46 +1,100 @@
-import tkinter as tk
-
-root = tk.Tk()
-root.title("Project Indigo (Beta)")
+import random
+import os
+from tkinter import *
 
 def get_posts():
     posts = []
-    try:
-        with open("posts.txt", "r") as f:
-            for line in f:
-                posts.append(line.strip())
-    except FileNotFoundError:
-        posts = []
+    with open("posts.txt", "r") as f:
+        for line in f:
+            posts.append(line.strip())
     return posts
 
 def post_message(message):
-    try:
-        with open("posts.txt", "a") as f:
-            f.write(message + "\n")
-    except FileNotFoundError:
-        print("Error: Could not open posts.txt")
+    with open("posts.txt", "a") as f:
+        f.write(message + "\n")
 
-def send_dm(username, message):
-    try:
-        with open("dms.txt", "a") as f:
-            f.write(f"{username}: {message}\n")
-    except FileNotFoundError:
-        print("Error: Could not open dms.txt")
+def send_dm(sender_number, recipient_number, message):
+    with open("dms.txt", "a") as f:
+        f.write(f"{sender_number}: {recipient_number}: {message}\n")
 
-def show_posts():
-    posts = get_posts()
-    for post in posts:
-        label = tk.Label(root, text=post)
-        label.pack()
+def get_username(number):
+    with open("accounts.txt", "r") as f:
+        for line in f:
+            if number == line.split(":")[0]:
+                return line.split(":")[1]
+    return None
+
+def identification_page():
+    number = generate_number()
+
+    with open("identification.txt", "w") as f:
+        f.write(str(number))
+    print(f"Your identification number is: {number}")
+
+def generate_number():
+    return random.randint(0, 999)
 
 def main():
-    show_posts()
+    root = Tk()
+    root.title("Project Indigo")
 
-    button = tk.Button(root, text="Post Message", command=lambda: post_message(input("Enter a message: ")))
-    button.pack()
+    # Create the posts list
+    posts = get_posts()
 
-    button = tk.Button(root, text="Send DM", command=lambda: send_dm(input("Enter a username: "), input("Enter a message: ")))
-    button.pack()
+    # Create the friends list
+    friends_list = []
+
+    def show_posts():
+        # Clear the posts list
+        posts_list.delete(0, END)
+
+        # Add the posts to the list
+        for post in posts:
+            # Add the user's number to the post
+            post = f"{post} - User{post.split('-')[0]}"
+            posts_list.insert(END, post)
+
+    def make_post():
+        message = input("Enter your message: ")
+        post_message(message)
+        show_posts()
+
+    def send_dm():
+        sender_number = input("Enter your number: ")
+        recipient_number = input("Enter the recipient's number: ")
+        message = input("Enter your message: ")
+        send_dm(sender_number, recipient_number, message)
+
+    def identification_page():
+        number = generate_number()
+
+        with open("identification.txt", "w") as f:
+            f.write(str(number))
+        print(f"Your identification number is: {number}")
+
+    def add_friend():
+        friend_number = input("Enter the friend's number: ")
+        friends_list.append(friend_number)
+
+    # Create the posts list
+    posts_list = Listbox(root)
+    posts_list.pack()
+
+    # Create the buttons
+    show_posts_button = Button(root, text="Show Posts", command=show_posts)
+    show_posts_button.pack()
+
+    make_post_button = Button(root, text="Make Post", command=make_post)
+    make_post_button.pack()
+
+    send_dm_button = Button(root, text="Send DM", command=send_dm)
+    send_dm_button.pack()
+
+    identification_button = Button(root, text="Identification", command=identification_page)
+    identification_button.pack()
+
+    friends_list_button = Button(root, text="Friends List", command=add_friend)
+    friends_list_button.pack()
 
     root.mainloop()
 
